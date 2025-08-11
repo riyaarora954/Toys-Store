dc4f785e680112dcbecad10e9e426d3ccf40bf7b
Riya Arora <arorariya954@gmail.com>
2025-08-11 18:27:03 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const fileUpload = require("express-fileupload");
-const cookieParser = require("cookie-parser");
 
 const adminRouter = require("./routes/admin.js");
 const userRouter = require("./routes/user.js");
 const indexRouter = require("./routes/index.js");
-const conn = require("./dbConfig/connection.js");
+// const conn = require("./dbConfig/connection.js");
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "fallback_secret";
+const cookieParser = require("cookie-parser");
 
+const JWT_SECRET = "xyz@900";
 const app = express();
-
-// Static + Views
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-// Middleware order matters
-app.use(cookieParser());
-app.use(fileUpload({})); // must come before urlencoded/json for multipart
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routers
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/", indexRouter);
 app.use("/user", userRouter);
@@ -31,7 +27,7 @@ app.use("/user", userRouter);
 const port = 3000;
 app.listen(port, (e) => {
   if (e) {
-    console.error(e);
+    console.log(e);
   } else {
     console.log(`Server is running on http://localhost:${port}`);
   }
